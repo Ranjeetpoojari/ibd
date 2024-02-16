f7129489daa22a84dc3731a9cddb23901eb9ac5c
Ranjeet poojari <ranjeetpoojari77@gmail.com>
2024-02-16 12:50:06 +0530

    update14/02/2024

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 $(document).ready(function () {
-    $("#outer-slider").owlCarousel({
-      items: 1,
+  $("#outer-slider").owlCarousel({
+    dots:false,
       loop: true,
-      nav:false,
-      autoWidth:true,
-      // Add other options for the outer carousel as needed
-    });
-
-    $("#inner-slider").owlCarousel({
-      items: 1,
-      loop: true,
-      nav:false,
-      autoWidth:true,
-      // Add other options for the inner carousel as needed
-    });
+      nav: true,
+      autoWidth: true,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 3
+          },
+          1000: {
+              items: 5
+          } // No comma here
+      },  navText: ['<i class="bi bi-arrow-left-short  rounded-circle bg-gray fs_30 fw_500 bg-white"></i>', '<i class="bi bi-arrow-right-short fs_30 fw_500 rounded-circle bg-white"></i>']
   });
+
+  $(".inner_slider").owlCarousel({
+      items: 1,
+      loop: true,
+      nav: false,
+      autoWidth: true,
+  });
+});
+
+  $('.service').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
